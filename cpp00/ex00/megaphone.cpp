1bbe2897612050d7d5acf1184ec15466848478e2
Dardan <dardangerguri@outlook.com>
2023-11-24 11:11:37 +0200

    Minor change

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:49:56 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/11/23 16:45:06 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/11/23 20:46:54 by dardangergu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv) {
 	if (argc != 1) {
 		for (int i = 1; i < argc; i++)
 			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)toupper(argv[i][j]);
+				std::cout << (char)std::toupper(argv[i][j]);
 			std::cout << std::endl;
 	}
 	else
