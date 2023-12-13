318ae56ab41088042f18ff99de863bac2e4842c8
Dardan <dardangerguri@outlook.com>
2023-12-13 16:15:38 +0200

    Added some newlines in the main

--- cpp01/ex05/srcs/main.cpp
+++ cpp01/ex05/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 17:59:53 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/12/01 14:48:01 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/12/13 16:13:13 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,13 @@ int	main(void) {
 	Harl		harl;
 
 	harl.complain("DEBUG");
+	std::cout << std::endl;
 	harl.complain("INFO");
+	std::cout << std::endl;
 	harl.complain("WARNING");
+	std::cout << std::endl;
 	harl.complain("ERROR");
+	std::cout << std::endl;
 	harl.complain("WRONG");
 	return (0);
 }
-// int	main(void) {
-// 	Harl		harl;
-// 	std::string	level;
-
-// 	std::cout 	<< "DEBUG, INFO, WARNING, ERROR, EXIT" << std::endl;
-// 				<< "Please enter the level: > ";
-// 	std::getline(std::cin, level);
-// 	while (level != "EXIT")
-// 	{
-
-// 	}
-// }
