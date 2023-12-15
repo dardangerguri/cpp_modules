7b908ef03f325ac2b51e41c53750d6d4e9eb3a00
Dardan <dardangerguri@outlook.com>
2023-12-15 18:24:16 +0200

    cleaned weapon class a little bit

--- cpp01/ex03/srcs/Weapon.cpp
+++ cpp01/ex03/srcs/Weapon.cpp
@@ -6,19 +6,16 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:58:27 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/11/29 14:16:24 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/12/15 18:23:21 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-Weapon::Weapon(std::string type) {
-	this->type = type;
-	return ;
+Weapon::Weapon(std::string type) : type(type) {
 }
 
 Weapon::~Weapon(void) {
-	return ;
 }
 
 const std::string	&Weapon::getType(void) const {
