442257fa1fd3ffe18972b6618f2564fd3863c4ab
Dardan <dardangerguri@outlook.com>
2023-12-22 12:44:18 +0200

    improved the main

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
+/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:27:46 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/12/22 11:05:55 by dardangergu      ###   ########.fr       */
+/*   Updated: 2023/12/22 12:38:51 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,37 @@ int	main( void ) {
 	std::cout << "----------------------------------------" << std::endl;
 
 	std::cout << YELLOW << "Comparison operators tests" << RESET << std::endl;
-	std::cout << Fixed(5) << " > " << Fixed(10) << " = " << (Fixed(5) > Fixed(10)) << std::endl;
-	std::cout << Fixed(5) << " < " << Fixed(10) << " = " << (Fixed(5) < Fixed(10)) << std::endl;
-	std::cout << Fixed(5) << " >= " << Fixed(10) << " = " << (Fixed(5) >= Fixed(10)) << std::endl;
-	std::cout << Fixed(5) << " <= " << Fixed(10) << " = " << (Fixed(5) <= Fixed(10)) << std::endl;
-	std::cout << Fixed(5) << " == " << Fixed(10) << " = " << (Fixed(5) == Fixed(10)) << std::endl;
-	std::cout << Fixed(5) << " != " << Fixed(10) << " = " << (Fixed(5) != Fixed(10)) << std::endl << std::endl;
+	Fixed n1(5);
+	Fixed n2(10);
+	std::cout << n1 << " > " << n2 << " = " << (n1 > n2) << std::endl;
+	std::cout << n1 << " < " << n2 << " = " << (n1 < n2) << std::endl;
+	std::cout << n1 << " >= " << n2 << " = " << (n1 >= n2) << std::endl;
+	std::cout << n1 << " <= " << n2 << " = " << (n1 <= n2) << std::endl;
+	std::cout << n1 << " == " << n2 << " = " << (n1 == n2) << std::endl;
+	std::cout << n1 << " != " << n2 << " = " << (n1 != n2) << std::endl << std::endl;
 
 	std::cout << RED << "Arithmetic operators tests" << RESET << std::endl;
-	std::cout << Fixed(5) << " + " << Fixed(5) << " = " << (Fixed(5) + Fixed(5)) << std::endl;
-	std::cout << Fixed(5) << " - " << Fixed(5) << " = " << (Fixed(5) - Fixed(5)) << std::endl;
-	std::cout << Fixed(5) << " * " << Fixed(5) << " = " << (Fixed(5) * Fixed(5)) << std::endl;
-	std::cout << Fixed(5) << " / " << Fixed(5) << " = " << (Fixed(5) / Fixed(5)) << std::endl << std::endl;
+	Fixed	nbr1(20);
+	Fixed	nbr2(2);
+	std::cout << nbr1 << " + " << nbr2 << " = " << nbr1 + nbr2 << std::endl;
+	std::cout << nbr1 << " - " << nbr2 << " = " << nbr1 - nbr2 << std::endl;
+	std::cout << nbr1 << " * " << nbr2 << " = " << nbr1 * nbr2 << std::endl;
+	std::cout << nbr1 << " / " << nbr2 << " = " << nbr1 / nbr2 << std::endl << std::endl;
 
 	std::cout << BLUE << "Min and Max tests" << RESET << std::endl;
-	std::cout << "Min(" << Fixed(5) << ", " << Fixed(10) << ") = " << Fixed::min(Fixed(5), Fixed(10)) << std::endl;
-	std::cout << "Max(" << Fixed(5) << ", " << Fixed(10) << ") = " << Fixed::max(Fixed(5), Fixed(10)) << std::endl;
-	std::cout << "Min(" << Fixed(5) << ", " << Fixed(10) << ") = " << Fixed::min(Fixed(5), Fixed(10)) << std::endl;
-	std::cout << "Max(" << Fixed(5) << ", " << Fixed(10) << ") = " << Fixed::max(Fixed(5), Fixed(10)) << std::endl << std::endl;
+	Fixed	nb1(5);
+	Fixed	nb2(10);
+	std::cout << "Min(" << nb1 << ", " << nb2 << ") = " << Fixed::min(nb1, nb2) << std::endl;
+	std::cout << "Max(" << nb1 << ", " << nb2 << ") = " << Fixed::max(nb1, nb2) << std::endl;
+	std::cout << "Min(" << nb1 << ", " << nb2 << ") = " << Fixed::min(nb1, nb2) << std::endl;
+	std::cout << "Max(" << nb1 << ", " << nb2 << ") = " << Fixed::max(nb1, nb2) << std::endl << std::endl;
 
 	std::cout << GREEN << "Increment/Decrement operators tests" << RESET << std::endl;
-	std::cout << "++" << Fixed(5) << " = " << ++Fixed(5) << std::endl;
-	std::cout << Fixed(5) << "++ = " << Fixed(5)++ << std::endl;
-	std::cout << "--" << Fixed(5) << " = " << --Fixed(5) << std::endl;
-	std::cout << Fixed(5) << "-- = " << Fixed(5)-- << std::endl << std::endl;
+	Fixed	number(5);
+	std::cout << "++" << number << " = " << ++number << std::endl;
+	std::cout << number << "++ = " << number++ << std::endl;
+	std::cout << "--" << number << " = " << --number << std::endl;
+	std::cout << number << "-- = " << number-- << std::endl << std::endl;
 
 	return 0;
 }
