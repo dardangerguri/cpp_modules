ffaaa0cfae9d6b179652bc3820936bddefdc7a75
Dardan <dardangerguri@outlook.com>
2023-12-28 16:47:05 +0200

    added the files

---
+++ cpp04/ex03/includes/Ice.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 17:59:46 by dgerguri          #+#    #+#             */
+/*   Updated: 2023/12/28 14:22:03 by dardangergu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include <iostream>
+# include "ICharacter.hpp"
+
+class	Character : public ICharacter {
+
+	public:
+
+		Character(void);
+		Character(std::string const & name);
+		Character(Character const & copy);
+		~Character(void);
+
+		Character & operator=(const Character & copy);
+
+		void setName(std::string const & name);
+		std::string const & getName(void) const;
+
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
+
+	private:
+
+		std::string		name;
+		AMateria*		inventory[4];
+};
+
+# endif
---
+++ cpp04/ex03/srcs/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 16:16:26 by dgerguri          #+#    #+#             */
+/*   Updated: 2023/12/28 16:19:44 by dgerguri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+#include "Character.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+#include "MateriaSource.hpp"
+
+int main()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+    me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	return 0;
+}
