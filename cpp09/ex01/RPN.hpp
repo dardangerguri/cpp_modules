a780ea962f73092a958f5b4917db03f821df45ef
Dardan <dardangerguri@outlook.com>
2024-02-08 11:56:41 +0200

    started rpn exercise

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 16:06:49 by dgerguri          #+#    #+#             */
+/*   Updated: 2024/02/08 11:54:37 by dardangergu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# define	GREEN	"\033[0;32m"
+# define	RED		"\033[0;31m"
+# define	BLUE	"\033[0;34m"
+# define	YELLOW	"\033[0;33m"
+# define	RESET	"\033[0m"
+
+
+# include <iostream>
+
+class RPN {
+
+	public:
+
+	private:
+
+
+}
+
+# endif
