28faba59e935e0b362cd4579611cf863065c7cb7
Dardan <dardangerguri@outlook.com>
2024-02-16 17:08:42 +0200

    changed the error handling now the program doesnt stop once an error is found

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
+/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 16:06:49 by dgerguri          #+#    #+#             */
-/*   Updated: 2024/02/16 09:42:05 by dardangergu      ###   ########.fr       */
+/*   Updated: 2024/02/16 12:28:59 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,72 +17,6 @@ BitcoinExchange::BitcoinExchange(void) {
 }
 
 BitcoinExchange::~BitcoinExchange(void) {
-}
-
-time_t	BitcoinExchange::convertStringToTimeT(std::string date) {
-	std::tm tm = {};
-    std::istringstream ss(date);
-    ss >> std::get_time(&tm, "%Y-%m-%d");
-    if (ss.fail())
-        return -1;
-    time_t t = mktime(&tm);
-	std::string const temp_date = convertTimeTToDate(t);
-	if (temp_date.compare(date))
-		return (-1);
-	return t;
-}
-
-double BitcoinExchange::convertStringToDouble(std::string value) {
-	if (value.empty() || std::count(value.begin(), value.end(), '.') > 1 ||
-		std::count(value.begin(), value.end(), '-') > 1 ||
-		!std::all_of(value.begin(), value.end(), [](char c) { return std::isdigit(c) || c == '.' || c == '-';}))
-		return (-2);
-	double result = std::stod(value);
-	if (result < 0)
-		return (-1);
-	return (result);
-}
-
-std::string	BitcoinExchange::convertTimeTToDate(time_t date) {
-	char buffer[80];
-	strftime(buffer, 80, "%Y-%m-%d", localtime(&date));
-	return buffer;
-}
-
-void	BitcoinExchange::displayBitcoinValue(time_t date, double value) {
-		std::map<time_t, double>::iterator closestDate = exchangeRatesByDates.lower_bound(date);
-			double closestValue = std::prev(closestDate)->second;
-		if (value > 0 && value < 1000) {
-			std::cout << convertTimeTToDate(date) << " => " << value << " = " << closestValue * value << std::endl;
-		}
-		else if (value <= 0) {
-			std::cerr << RED "Error: not a positive number." RESET << std::endl;
-		}
-		else if (value >= 1000) {
-			std::cerr << RED "Error: too large a number." RESET << std::endl;
-		}
-}
-
-void	BitcoinExchange::calculateBitcoinValue(char *filename) {
-		std::ifstream file(filename);
-		if (!file.is_open())
-			throw BitcoinExchange::InvalidFile();
-		std::string line;
-		std::getline(file, line);
-		if (line.compare("date | value") != 0)
-			std::cout << RED "Warning: the file title is not correct." RESET << std::endl;
-		while (std::getline(file, line)) {
-			time_t date = convertStringToTimeT(line.substr(0, line.find(" |")));
-			double value = convertStringToDouble(line.substr(line.find("|") + 1));
-			std::cout << GREEN << value << RESET << std::endl;
-			if (date != -1 && value != -2)
-				displayBitcoinValue(date, value);
-			else if (date == -1)
-				std::cout << RED "Error: bad input => " << line.substr(0, line.find(" |")) << RESET << std::endl;
-			else
-				std::cout << RED "Error: bad input => " << line.substr(line.find("|") + 1) << RESET << std::endl;
-		}
-		file.close();
 }
 
 void	BitcoinExchange::loadDataFromFile(void) {
@@ -110,6 +44,80 @@ void	BitcoinExchange::loadDataFromFile(void) {
 	catch (std::exception & e) {
 		throw BitcoinExchange::InvalidFile();
 	}
+}
+
+void	BitcoinExchange::calculateBitcoinValue(char *filename) {
+		std::ifstream file(filename);
+		if (!file.is_open())
+			throw BitcoinExchange::InvalidFile();
+		std::string line;
+		std::getline(file, line);
+		if (line.compare("date | value") != 0)
+			std::cout << RED "Warning: the file title is not correct." RESET << std::endl;
+		while (std::getline(file, line)) {
+			time_t date;
+			double value;
+			try {
+				date = convertStringToTimeT(line.substr(0, line.find(" |")));
+				value = convertStringToDouble(line.substr(line.find("|") + 1));
+			}
+			catch (std::exception & e) {
+				if (date != -1)
+					std::cout << RED "Error: bad input => " << line.substr(line.find("|") + 1) << RESET << std::endl;
+				if (date == -1)
+					std::cout << RED "Error: bad input => " << line.substr(0, line.find("|")) << RESET << std::endl;
+				continue;
+			}
+			if (date == -1)
+				std::cout << RED "Error: bad input => " << line.substr(0, line.find("|")) << RESET << std::endl;
+			if (date != -1)
+				displayBitcoinValue(date, value);
+		}
+		file.close();
+}
+
+time_t	BitcoinExchange::convertStringToTimeT(std::string date) {
+	std::tm tm = {};
+    std::istringstream ss(date);
+    ss >> std::get_time(&tm, "%Y-%m-%d");
+    if (ss.fail())
+        return -1;
+    time_t t = mktime(&tm);
+	std::string const temp_date = convertTimeTToDate(t);
+	if (temp_date.compare(date))
+		return (-1);
+	return t;
+}
+
+std::string	BitcoinExchange::convertTimeTToDate(time_t date) {
+	char buffer[80];
+	strftime(buffer, 80, "%Y-%m-%d", localtime(&date));
+	return buffer;
+}
+
+void	BitcoinExchange::displayBitcoinValue(time_t date, double value) {
+		std::map<time_t, double>::iterator closestDate = exchangeRatesByDates.lower_bound(date);
+			double closestValue = std::prev(closestDate)->second;
+		if (value > 0 && value < 1000) {
+			std::cout << convertTimeTToDate(date) << " => " << value << " = " << closestValue * value << std::endl;
+		}
+		else if (value <= 0) {
+			std::cerr << RED "Error: not a positive number." RESET << std::endl;
+		}
+		else if (value >= 1000) {
+			std::cerr << RED "Error: too large a number." RESET << std::endl;
+		}
+}
+
+double BitcoinExchange::convertStringToDouble(std::string value) {
+	std::stringstream s;
+	s << value;
+	double n;
+	while (!(s >> n) || s.fail() || !s.eof())
+	{
+		throw std::runtime_error("Error: Invalid value");
+	}
+	return (n);
 }
 
 char const *BitcoinExchange::InvalidFile::what() const throw() {
